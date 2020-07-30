641182c408203956e4181f1256bd3d5e97c068db
khanhvph05@gmail.com <khanhvph05@gmail.com>
2020-07-30 09:08:59 +0700

    init project

---
+++ app/dao.py
@@ -0,0 +1,17 @@
+import json
+import os
+from app import app
+
+def read_products(category_id=0):
+    with open(os.path.join(app.root_path, "data/products.json"), encoding="utf-8") as f:
+        products =  json.load(f)
+
+        if category_id > 0:
+            products = [p for p in products if p["category_id"] == category_id]
+            # cach truyen thong
+            # results = []
+            # for p in products:
+            #     if p["category_id"] == category_id:
+            #         results.append()
+        return products
+
