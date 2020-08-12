cbb7f5d099056cef1786416bb8f4a69b65118ccb
= <=>
2020-08-13 02:21:09 +0700

    update code for 5,6 requirement

--- app/dao.py
+++ app/dao.py
@@ -2,12 +2,29 @@ import json
 import os
 from app import app
 
-def read_products(category_id=0):
+
+def read_product_id(product_id):
+    products = read_products()
+    for p in products:
+        if p["id"] == product_id:
+            return p
+
+    return None
+
+
+def read_products(category_id=0, keyword=None, from_price=None, to_price=None):
     with open(os.path.join(app.root_path, "data/products.json"), encoding="utf-8") as f:
-        products =  json.load(f)
+        products = json.load(f)
 
         if category_id > 0:
             products = [p for p in products if p["category_id"] == category_id]
+
+        if keyword:
+            products = [p for p in products if p["name"].lower().find(keyword.lower()) >= 0]
+
+        if from_price and to_price:
+            products = [p for p in products if p["price"] >= float(from_price) and p["price"] <= float(to_price)]
+
             # cach truyen thong
             # results = []
             # for p in products:
@@ -15,3 +32,58 @@ def read_products(category_id=0):
             #         results.append()
         return products
 
+
+def update_product(product_id, name, description, price, images, category_id):
+    products = read_products()
+    for idx, p in enumerate(products):
+        if p["id"] == int(product_id):
+            products[idx]["name"] = name
+            products[idx]["description"] = description
+            products[idx]["price"] = float(price)
+            products[idx]["images"] = images
+            products[idx]["category_id"] = int(category_id)
+
+            break
+
+    try:
+        with open(os.path.join(app.root_path, "data/products.json"),
+                  "w", encoding="utf-8") as f:
+            json.dump(products, f, ensure_ascii=False, indent=4)
+
+            return True
+    except Exception as ex:
+        print(ex)
+        return False
+
+
+def add_product(name, description, price, images, category_id):
+    products = read_products()
+    product = {
+        "id": len(products) + 1,
+        "name": name,
+        "description": description,
+        "price": float(price),
+        "images": images,
+        "category_id": int(category_id)
+    }
+    products.append(product)
+
+    try:
+        with open(os.path.join(app.root_path, "data/products.json"),
+                  "w", encoding="utf-8") as f:
+            json.dump(products, f, ensure_ascii=False, indent=4)
+            # indent giup format json ngay hang thang loi
+
+            return True
+    except Exception as ex:
+        print(ex)
+        return False
+
+
+def read_categories():
+    with open(os.path.join(app.root_path, "data/categories.json"), encoding="utf-8") as f:
+        return json.load(f)
+
+
+if __name__ == "__main__":
+    print(read_products())
--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from app import app, dao
 
 
@@ -6,13 +6,66 @@ from app import app, dao
 def index():
     return render_template("index.html")
 
+
 @app.route("/products")
 def products_list():
-    return render_template("products.html", products=dao.read_products())
+    # kw = request.args("keyword")
+    # how difference? neu co truyen keyword thi lay len con ko thi se lay None
+    # => khong bi loi
+    kw = request.args.get("keyword")
+    from_price = request.args.get("from_price")
+    to_price = request.args.get("to_price")
+
+    return render_template("products.html",
+                           products=dao.read_products(keyword=kw,
+                                                      from_price=from_price,
+                                                      to_price=to_price))
+
 
 @app.route("/products/<int:category_id>")
 def products_by_cat_id(category_id):
-    return render_template("products.html", products=dao.read_products(category_id=category_id))
+    return render_template("products.html",
+                           products=dao.read_products(category_id=category_id))
+
+
+@app.route("/products/add", methods=["get", "post"])
+def add_or_update_product():
+    err = ""
+    product_id = request.args.get("product_id")
+    product = None
+    # if request.method == "POST":
+    if request.method.lower() == "post":
+        # name = request.form.get("name")
+        # price = request.form.get("price", 0)
+        # images = request.form.get("images")
+        # description = request.form.get("description")
+        # categories_id = request.form.get("categories_id", 0)
+        import pdb  # 2 dong nay dung de
+        # pdb.set_trace()  # debug
+
+        if product_id:  # Cap nhat
+            data = dict(request.form.copy())
+            data["product_id"] = product_id
+            if dao.update_product(**data):
+                return redirect(url_for("products_list"))
+        else:  # Them
+            if dao.add_product(**dict(request.form)):
+                # if dao.add_product(name=name, price=price, images=images,
+                #                    description=description, categories_id=categories_id):
+                return redirect(url_for("products_list"))
+
+        err = "Something wrong!!! Please back later!!!"
+
+    product_id = request.args.get("product_id")
+    product = None
+    if product_id:
+        product = dao.read_product_id(product_id=int(product_id))
+
+    return render_template("product-add.html",
+                           categories=dao.read_categories(),
+                           product=product,
+                           err=err)
+
 
 if __name__ == "__main__":
     app.run(debug=False)
